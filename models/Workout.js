c9f0575714d9d5a5e53cc0db71055f89b129ebd1
Erich Purpur <epurpur@gmail.com>
2021-06-27 08:22:04 -0400

    creating workout model schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,3 +1,43 @@
-module.exports = {
-    Workout: require('./Workout')
-}
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+//name, type, weight, sets, reps, and duration of exercise. 
+// If the exercise is a cardio exercise, I should be able to track my distance traveled.
+
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        name: {
+            type: String,
+            required: "Workout name is required"
+        },
+        type: {
+            type: String,
+            unique: false,
+            required: "Workout type is required"
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
